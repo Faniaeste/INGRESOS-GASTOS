3430974e90913a9efa9d54503fadeed81a8fadc1
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-05 16:07:35 +0100

    Lectura de csv y añadido a lista de index.

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -1,13 +1,22 @@
 from app_ingresos_gastos import app
 from flask import render_template
+import csv
 
 @app.route("/")
 def index():
-    datos = [
-        {'Fecha':'01/09/2026':'concepto':'Salario','monto':1800},
-        {'Fecha':'01/09/2026':'concepto':'Salario','monto':1800},
-        {'Fecha':'01/09/2026':'concepto':'Salario','monto':1800},
+    datos = []
+    fichero = open('app_ingresos_gastos/data/movimientos.csv','r')
+    lectura = csv.reader(fichero,delimiter=",",quotechar='"')
+    for item in lectura:
+        datos.append(item)
+
+    """
+        datos=[
+        {'Fecha':'01/09/2026','concepto':'Salario','monto':1800},
+        {'Fecha':'01/09/2026','concepto':'Salario','monto':1800},
+        {'Fecha':'01/09/2026','concepto':'Salario','monto':1800},
     ]
+    """
     return render_template("index.html", title = "Lista")
 
 @app.route("/new")
